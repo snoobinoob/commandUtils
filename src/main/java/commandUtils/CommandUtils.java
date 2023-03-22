260b36c5daa17258b0c6a7299954500261eb100f
Snoobinoob <cheeseycheese99@gmail.com>
2023-03-21 20:39:11 -0400

    Initial commit

---
+++ src/main/java/commandUtils/CommandUtils.java
@@ -0,0 +1,11 @@
+package commandUtils;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class CommandUtils {
+
+    public void init() {
+        System.out.println("Hello world from Command Utils!");
+    }
+}
